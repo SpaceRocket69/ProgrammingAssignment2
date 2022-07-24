87ab263bc941645e24e514f8f39fc142ac6b20d6
Maurice Erpelding <maurice.erpelding@smail.inf.h-brs.de>
2022-07-24 18:09:56 +0200

    Addet more features

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,43 @@
-
 ## Creates a matrix and safes an inverse of the matrix after
 ## the function 'cacheSolve' is executed
-
 makeCacheMatrix <- function(x = matrix()) {
-  
-  m <<- x
-  
-  inverseMatrix <<- NULL
-  
-  return(m)
-  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInvertion <- function(solve) m <<- solve
+  getInvertion <- function() m
+  list(set = set, get = get,
+       setInvertion = setInvertion,
+       getInvertion = getInvertion)
 }
-
 
 ## Inverts the matrix which is given to the 'makeCacheMatrix' function
-
 cacheSolve <- function(x, ...) {
+  m <- x$getInvertion()
   
-  if(!is.null(inverseMatrix)) {
+  ## Checks if the given matrix is different to the previous
+  ## and if the inverse matrix is already computed
+  if(!is.null(m)) {
     message("Getting cached data")
-    return(inverseMatrix)
+    return(m)
   }
-  
-  inverseMatrix <<- solve(m)
-  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInvertion(m)
+  m
 }
+
+## Testing:
+
+# mm1 <<- matrix(sample.int(9, 9, replace = FALSE), nrow = 3, ncol = 3)
+# mm2 <<- matrix(sample.int(10:18, 9, replace = FALSE), nrow = 3, ncol = 3)
+# aMatrix <- makeCacheMatrix(mm1)
+# aMatrix$get()                     # retrieve the value of x
+# aMatrix$getInvertion()            # retrieve the value of m, which should be NULL
+# aMatrix$set(mm2)                  # reset value with a new matrix
+# cacheSolve(aMatrix)               # notice invertion calculated is invertion of mm2, not mm1
+# aMatrix$getInvertion()            # retrieve it directly, now that it has been cached
+
